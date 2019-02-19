e0425f612461a520cf495b0c43a783729374afc9
malvin <malvinsutanto@gmail.com>
2019-02-19 22:59:54 +0900

    Add comments

--- annotation/src/main/java/com/malvinstn/annotation/AnalyticsEvent.kt
+++ annotation/src/main/java/com/malvinstn/annotation/AnalyticsEvent.kt
@@ -1,5 +1,9 @@
 package com.malvinstn.annotation
 
+// Set Target as class since we want this annotation to be added to a class element
 @Target(AnnotationTarget.CLASS)
+// Set retention as Source since we only need this annotation
+// during annotation processing process and
+// we don't need this class at runtime.
 @Retention(AnnotationRetention.SOURCE)
 annotation class AnalyticsEvent
